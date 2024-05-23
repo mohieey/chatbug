e65ffed2b0dacc00d0e6cdf376596bdb4250df61
mohieey <Mohhiey@gmail.com>
2024-05-24 01:30:56 +0300

    create user model

--- chatbugRails/db/schema.rb
+++ chatbugRails/db/schema.rb
@@ -10,5 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_23_222723) do
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
