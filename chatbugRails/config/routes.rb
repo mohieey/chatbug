c9737663f56a59267d8831b7c01eb60a1ace75c0
mohieey <Mohhiey@gmail.com>
2024-05-24 06:17:14 +0300

    modify routes

--- chatbugRails/config/routes.rb
+++ chatbugRails/config/routes.rb
@@ -4,11 +4,13 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  scope :api do
-    scope :users do
-      post :signup, to: 'users#sign_up'
-      post :signin, to: 'users#sign_in'
-    end
+  scope :users do
+    post :signup, to: 'users#sign_up'
+    post :signin, to: 'users#sign_in'
+  end
+
+  scope :applications do
+    post :create, to: 'applications#create'
   end
 
   # Defines the root path route ("/")
