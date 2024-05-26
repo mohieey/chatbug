b960b3ee684a7bca9752f1ded311f809505bb0f8
mohieey <Mohhiey@gmail.com>
2024-05-26 05:08:03 +0300

    added listing messages per chat

---
+++ chatbugRails/app/controllers/messages_controller.rb
@@ -0,0 +1,30 @@
+class MessagesController < ApplicationController
+  before_action :authenticate_user, only: [:index]
+
+  def index
+    application_token = params[:application_token]
+    chat_number = params[:chat_number]
+
+    chat = Chat.find_by_application_token_and_chat_number(application_token, chat_number)
+    if chat.nil?
+      raise NotFoundError
+    end
+
+    messages = []
+    chat.messages.find_each do |message|
+      messages << decorate(message)
+    end
+
+    render json: messages, status: :ok
+  end
+
+
+  private
+
+  def decorate(message)
+    {
+      number: message.number,
+      text: message.text,
+    }
+  end
+end
--- chatbugRails/config/routes.rb
+++ chatbugRails/config/routes.rb
@@ -13,9 +13,11 @@ Rails.application.routes.draw do
     get :'', to: 'applications#index'
     post :create, to: 'applications#create'
 
-    scope '/:application_token' do
-      scope :chats do
-        get :'', to: 'chats#index'
+    scope '/:application_token/chats' do
+      get :'', to: 'chats#index'
+
+      scope '/:chat_number/messages' do
+        get :'', to: 'messages#index'
       end
     end
   end
